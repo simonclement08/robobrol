4d8e61f287917e8b7bc322cd64ac6f9216a33747
clement <simon.clement.pierre.daniel@gmail.com>
2022-05-31 10:16:57 +0200

    Affichage des caracteristiques d'un jeu

--- src/Form/VideoFormType.php
+++ src/Form/VideoFormType.php
@@ -6,7 +6,7 @@ use App\Entity\Video;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class VideoFormType extends AbstractType
@@ -14,9 +14,9 @@ class VideoFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('link', UrlType::class, [
+            ->add('link', TextType::class, [
                 'required' => true,
-                'label' => 'Lien',
+                'label' => 'ID de la vidéo Youtube',
                 'constraints' => [new Assert\Length(['max' => 255])],
             ]);
     }
